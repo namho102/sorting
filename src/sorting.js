a2fa7f1e4aedabe013e79869eaaaeaf87d08c0a5
Hoang Nam <namhoang.td09@gmail.com>
2016-02-04 21:07:15 +0700

    Update sorting.js

--- src/sorting.js
+++ src/sorting.js
@@ -5,7 +5,6 @@ Array.prototype.bubblesort = function () {
         for (var i = 1; i < this.length; i++) {
             if (this[i - 1] > this[i]) {
                 done = false;
-                // sleep(1000);
                 [this[i - 1], this[i]] = [this[i], this[i - 1]];
                 return this;
             }
